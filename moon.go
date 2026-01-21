fd9b28de6f04405244868f5a19686c62a633192a
Raven <alimateeee@gamil.com>
2026-01-21 14:38:32 +0330

    update configs , updating moon.go

--- moon.go
+++ moon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,24 +13,62 @@ func Moon(hostname string) {
 	fmt.Println("\nUsing Moon_starter")
 	fmt.Println("enabling normal mode ...")
 	time.Sleep(1 * time.Second)
-	fmt.Println("\nOptions : \n\t1.Plan =>    Show changes required by the current configuration \n\t2.apply =>   Create or update infrastructure \n\t3.destroy => Destroy previously-created infrastructure \n\t4.Exit")
+	fmt.Println("\nOptions : \n\t1.Enter Configuration =>    user configuration for VMs \n\t------------\t\n\t2.Plan =>    Show changes required by the current configuration \n\t------------\t\n\t3.apply =>   Create or update infrastructure \n\t------------\t\n\t4.destroy => Destroy previously-created infrastructure \n\t------------\t\n\t5.Exit")
 	fmt.Print("choice : ")
 	fmt.Scan(&userinput)
 	mode := "plan"
 	switch userinput {
 	case 1:
-		terraform_plan_moon(&mode)
+		userVariables()
 		return
 	case 2:
-		terraform_apply_moon(&mode)
+		terraform_plan_moon(&mode)
 		return
 	case 3:
-		terraform_destroy_moon(&mode)
+		terraform_apply_moon(&mode)
 		return
 	case 4:
+		terraform_destroy_moon(&mode)
+		return
+	case 5:
 		os.Exit(0)
 	}
 }
+
+func userVariables() {
+	var (
+		RAM int8
+		CPU int8
+	)
+	fmt.Print("RAM values : ")
+	fmt.Scan(&RAM)
+	fmt.Print("CPU core value : ")
+	fmt.Scan(&CPU)
+
+	valus := map[string]int{
+		"cpu_cores" : int(CPU),
+		"ram" : int(RAM),
+	}
+	data , err := json.MarshalIndent(valus , "" , " ")
+	if err != nil {
+		panic(data)
+	}
+	// getting working directory
+	currentDir , err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile(currentDir + "/normal/terraform.auto.tfvars.json" , data , 0644)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully written to terraform.auto.tfvars.json.")
+		fmt.Println("going back to the menu ...")
+		time.Sleep(1 * time.Second)
+	}
+	main()
+}
+
 
 func terraform_plan_moon(mode *string) {
 	fmt.Printf("command ==> terraform %v ==> executing ...\n\n", *mode)
